b83ccd4bed695be729aa7303641182a22e5e5376
Prasanth Ganesan <vvmsanth56@gmail.com>
2019-03-25 12:20:59 +0530

    feat: Player name passed to game screen

--- app/src/main/java/com/vvmp/coinprediction/util/Key.kt
+++ app/src/main/java/com/vvmp/coinprediction/util/Key.kt
@@ -3,4 +3,7 @@ package com.vvmp.coinprediction.util
 object Key {
     // Intent Keys
     const val USER_NAME = "userName"
+    const val PLAYER_ONE_NAME = "playerOneName"
+    const val PLAYER_TWO_NAME = "playerTwoName"
+
 }
--- app/src/main/java/com/vvmp/coinprediction/view/NameActivity.kt
+++ app/src/main/java/com/vvmp/coinprediction/view/NameActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.vvmp.coinprediction.R
+import com.vvmp.coinprediction.util.Key
 import kotlinx.android.synthetic.main.activity_name.*
 
 class NameActivity : AppCompatActivity() {
@@ -14,8 +15,23 @@ class NameActivity : AppCompatActivity() {
 
         btn_action_next.setOnClickListener {
             val intent = Intent(this@NameActivity, GameActivity::class.java)
+
+            val playerOneName = edt_player_name_1?.text
+            intent.putExtra(Key.PLAYER_ONE_NAME, edt_player_name_1.text)
             startActivity(intent) // Launches the Game screen
             finish()
         }
+    }
+
+    fun getPlayerOneName(): String {
+        var name = edt_player_name_1?.text?.toString()
+        if (name == null) name = "John";
+        return name
+    }
+
+    fun getPlayerTwoName(): String {
+        var name = edt_player_name_1?.text?.toString()
+        if (name == null) name = "Jack";
+        return name
     }
 }
