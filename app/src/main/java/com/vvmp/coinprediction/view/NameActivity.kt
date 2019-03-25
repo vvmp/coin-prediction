62d38d1fb4bc19987a25567a6edf335aa81efcfa
Prasanth Ganesan <vvmsanth56@gmail.com>
2019-03-25 11:59:59 +0530

    feat: Updated the theme and removed the coin count screen

--- app/src/main/java/com/vvmp/coinprediction/view/NameActivity.kt
+++ app/src/main/java/com/vvmp/coinprediction/view/NameActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.vvmp.coinprediction.R
-import com.vvmp.coinprediction.util.Key
 import kotlinx.android.synthetic.main.activity_name.*
 
 class NameActivity : AppCompatActivity() {
@@ -14,17 +13,7 @@ class NameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_name)
 
         btn_action_next.setOnClickListener {
-            val intent = Intent(this@NameActivity, CoinActivity::class.java)
-            intent.putExtra(Key.USER_NAME, getName()) // Put the entered name into the Intent
-            startActivity(intent) // Launches the Coin count screen
-            finish()
+            // TODO - Navigate to the game Activity
         }
-    }
-
-    /*
-    Fetches name from EditText
-     */
-    private fun getName(): String {
-        return edit_text_username.text.toString()
     }
 }
